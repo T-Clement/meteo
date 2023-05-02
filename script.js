06427c48ed219c3738d15625188cdff6a6fed94e
Alexandre DUBREZ <adubrez1409@gmail.com>
2023-05-02 15:57:14 +0200

    add getForecastTo10Days function

--- script.js
+++ script.js
@@ -12,9 +12,21 @@ async function toGetValuesfromCity(name) {
         `https://api.weatherapi.com/v1/current.json?key=dfb545a573604021be494635230205&q=${name}&aqi=no`
     );
     let valuesAPI = await response.json();
-    console.log(valuesAPI.current.temp_c);
+    console.log(valuesAPI);
 }
 toGetValuesfromCity("Caen");
+
+// Forecast to 10 days
+// async function toGetValuesfromCity(name) {
+//     let response = await fetch(
+//         `https://api.weatherapi.com/v1/forecast.json?key=dfb545a573604021be494635230205&q=Caen&days=10&aqi=no&alerts=no`
+//     );
+//     let valuesAPI = await response.json();
+//     console.log(valuesAPI);
+// }
+// toGetValuesfromCity("Caen");
+
+// https://api.weatherapi.com/v1/forecast.json?key=dfb545a573604021be494635230205&q=Caen&days=10&aqi=no&alerts=no
 
 // let url="http://api.weatherapi.com/v1/search.json?key=dfb545a573604021be494635230205&q="
 // url+=location.name
