2df09fc53dbaf7ab5455a8f01ce8c026b0ae577d
Alexandre DUBREZ <adubrez1409@gmail.com>
2023-05-02 15:32:08 +0200

    add toGetValuesFromCity function

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ async function toGetCity(name) {
 }
 toGetCity("Wurzburg");
 
+async function toGetValuesfromCity(name) {
+    let response = await fetch(
+        `https://api.weatherapi.com/v1/current.json?key=dfb545a573604021be494635230205&q=${name}&aqi=no`
+    );
+    let valuesAPI = await response.json();
+    console.log(valuesAPI.current.temp_c);
+}
+toGetValuesfromCity("Caen");
+
 // let url="http://api.weatherapi.com/v1/search.json?key=dfb545a573604021be494635230205&q="
 // url+=location.name
 // return url
